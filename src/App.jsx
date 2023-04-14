1e6a58a124562a46d1f5ea3824c2a24a6e383674
Manish Gupta <121728309+DevyManish@users.noreply.github.com>
2023-04-14 16:24:08 +0530

    g2

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         <section className="text-gray-400 bg-gray-900 body-font">
             <div className="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
                 <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero"
-                    src="src\assets\img2.jpeg" />
+                    src={"/img2.jpeg"} />
                 <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Relish Free Video
                         Conferencing
