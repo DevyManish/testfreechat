8b5c5c714f3999f5984d6adc71ea1269bc18d0e7
Manish Gupta <121728309+DevyManish@users.noreply.github.com>
2023-04-14 15:31:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,149 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        {/*header starts*/}
+        <header className="text-gray-400 bg-gray-900 body-font">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                    <img className="logo-img" src="src\assets\logo.png" width={60} height={60} alt="logo" align="CENTER" />
+                    <span className="ml-3 text-xl">FreeChat</span>
+                </a>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <a className="mr-5 hover:text-white">Home</a>
+                    <a className="mr-5 hover:text-white">Services</a>
+                    <a className="mr-5 hover:text-white">Contact</a>
+                    <a className="mr-5 hover:text-white">About</a>
+                </nav>
+                <button
+                    className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                        className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7" />
+                    </svg>
+                </button>
+            </div>
+        </header>
+        {/*header ends*/}
+        {/* body starts */}
+        <section className="text-gray-400 bg-gray-900 body-font">
+            <div className="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
+                <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero"
+                    src="src\assets\img2.jpeg" />
+                <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Relish Free Video
+                        Conferencing
+                    </h1>
+                    <p className="mb-8 leading-relaxed">A web-based tool that enables users to connect with each other
+                        in
+                        real-time using video and audio technology.Easily connect with family,friends and colleagues at
+                        a
+                        simple click.</p>
+                    <div className="flex w-full justify-center items-end">
+                        <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
+                            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-400">Start or join a
+                                meeting</label>
+                            <input type="text" id="hero-field" name="hero-field"
+                                className="w-full bg-gray-800 rounded border bg-opacity-40 border-gray-700 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <button
+                            className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Go</button>
+                    </div>
+                    <p className="text-sm mt-2 text-gray-500 mb-8 w-full">Enter url to join or click on go</p>
+                    <div className="flex text-gray-300">
+                        <button
+                            className="bg-gray-800 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-700 hover:text-white focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-6 h-6"
+                                viewBox="0 0 512 512">
+                                <path
+                                    d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z">
+                                </path>
+                            </svg>
+                            <span className="ml-4 flex items-start flex-col leading-none">
+                                <span className="text-xs text-gray-400 mb-1">GET IT ON</span>
+                                <span className="title-font font-medium">Google Play</span>
+                            </span>
+                        </button>
+                        <button
+                            className="bg-gray-800 inline-flex py-3 px-5 rounded-lg items-center ml-4 hover:bg-gray-700 hover:text-white focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-6 h-6"
+                                viewBox="0 0 305 305">
+                                <path
+                                    d="M40.74 112.12c-25.79 44.74-9.4 112.65 19.12 153.82C74.09 286.52 88.5 305 108.24 305c.37 0 .74 0 1.13-.02 9.27-.37 15.97-3.23 22.45-5.99 7.27-3.1 14.8-6.3 26.6-6.3 11.22 0 18.39 3.1 25.31 6.1 6.83 2.95 13.87 6 24.26 5.81 22.23-.41 35.88-20.35 47.92-37.94a168.18 168.18 0 0021-43l.09-.28a2.5 2.5 0 00-1.33-3.06l-.18-.08c-3.92-1.6-38.26-16.84-38.62-58.36-.34-33.74 25.76-51.6 31-54.84l.24-.15a2.5 2.5 0 00.7-3.51c-18-26.37-45.62-30.34-56.73-30.82a50.04 50.04 0 00-4.95-.24c-13.06 0-25.56 4.93-35.61 8.9-6.94 2.73-12.93 5.09-17.06 5.09-4.64 0-10.67-2.4-17.65-5.16-9.33-3.7-19.9-7.9-31.1-7.9l-.79.01c-26.03.38-50.62 15.27-64.18 38.86z">
+                                </path>
+                                <path
+                                    d="M212.1 0c-15.76.64-34.67 10.35-45.97 23.58-9.6 11.13-19 29.68-16.52 48.38a2.5 2.5 0 002.29 2.17c1.06.08 2.15.12 3.23.12 15.41 0 32.04-8.52 43.4-22.25 11.94-14.5 17.99-33.1 16.16-49.77A2.52 2.52 0 00212.1 0z">
+                                </path>
+                            </svg>
+                            <span className="ml-4 flex items-start flex-col leading-none">
+                                <span className="text-xs text-gray-400 mb-1">Download on the</span>
+                                <span className="title-font font-medium">App Store</span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* body ends */}
+        {/*footer starts*/}
+        <footer className="text-gray-400 bg-gray-900 body-font">
+            <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+                <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+                    <img className="logo-img" src="src\assets\logo.png" width={40} height={40} alt="logo" align="CENTER" />
+                    <span className="ml-3 text-xl">FreeChat</span>
+                </a>
+                <p
+                    className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
+                    ©
+                    2023 FreeChat Web —
+                    <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank"
+                        rel="noopener noreferrer"> @ Lumina Tech </a>
+                </p>
+                <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <a className="text-gray-400">
+                        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                            className="w-5 h-5" viewBox="0 0 24 24">
+                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                        </svg>
+                    </a>
+                    <a className="ml-3 text-gray-400">
+                        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                            className="w-5 h-5" viewBox="0 0 24 24">
+                            <path
+                                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                            </path>
+                        </svg>
+                    </a>
+                    <a className="ml-3 text-gray-400">
+                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                            strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                            <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                        </svg>
+                    </a>
+                    <a className="ml-3 text-gray-400">
+                        <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                            strokeWidth={0} className="w-5 h-5" viewBox="0 0 24 24">
+                            <path stroke="none"
+                                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
+                            </path>
+                            <circle cx={4} cy={4} r={2} stroke="none" />
+                        </svg>
+                    </a>
+                </span>
+            </div>
+        </footer>
+        {/*footer ends*/}
+    </div>
+    </div>
+  )
+}
+
+export default App
